0af9f872d1e4cf7dd6dc4850aabcda0ab1d46f0f
Alwin Garside <alwin@dmp.nl>
2018-12-18 12:46:34 +0100

    Fix D1 SDK package name

--- generate-php.sh
+++ generate-php.sh
@@ -3,7 +3,6 @@
 docker-compose run openapi-generator generate \
       --generator-name  "php" \
       --config          "/local/openapi-config.php.json" \
-      --invoker-package "Bungie" \
       --input-spec      "https://raw.githubusercontent.com/Bungie-net/api/master/openapi.json" \
       --output          "/local/bungie-sdk-php" \
       -DapiTests=false \
@@ -11,8 +10,7 @@ docker-compose run openapi-generator generate \
 
 docker-compose run openapi-generator generate \
       --generator-name  "php" \
-      --config          "/local/openapi-config.php.json" \
-      --invoker-package "Bungie\D1" \
+      --config          "/local/openapi-config-d1.php.json" \
       --input-spec      "https://raw.githubusercontent.com/DestinyDevs/BungieNetPlatform/master/wiki-builder-v2/data/openapi-d1.json" \
       --output          "/local/bungie-d1-sdk-php" \
       -DapiTests=false \
